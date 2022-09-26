8b859ae26fc771332526dc1e8bdac5778541329b
lopkn <leoyuan06@gmail.com>
2022-09-26 21:52:17 +0800

    feat: physics simulator update

--- public/physics/physicsP5.js
+++ public/physics/physicsP5.js
@@ -43,10 +43,16 @@ class PHOBJ{
     this.vy+=0.01
     this.rotation = turn(this.rotation,this.r)
     
+    let comps = vectorFuncs.ShComp(this.vx,this.vy,0,1)
     
-    let momentum = distance(0,0,this.vx,this.vy)
+    // console.log(comps)
+    let momentum = comps[2][0]
+    
+    // let momentum = distance(0,0,this.vx,this.vy)
     
     let notePts = []
+    
+    let Cancelables = [0,0,0]
     
     this.points.forEach((e,i)=>{
       let rx = (e[0] * this.rotation[1] + e[1] * this.rotation[0])
@@ -58,8 +64,11 @@ class PHOBJ{
       if(yy > 600){
         notePts.push([xx,yy,rx,ry])
         
-        this.applyForceInstance(rx,ry,0,-momentum*2)
-        
+        let V = this.applyForceInstance(rx,ry,0,-momentum*2)
+        Cancelables[0] += V[0]
+        Cancelables[1] += V[1]
+        Cancelables[2] += V[2]
+        // console.log(V[2])
       }
       
     })
@@ -70,6 +79,10 @@ class PHOBJ{
       }
     })
     
+    this.vx += Cancelables[0]
+    this.vy += Cancelables[1]
+    this.r -= Cancelables[2]
+    
     
   }
   
@@ -77,18 +90,25 @@ class PHOBJ{
   draw(){
     this.lines.forEach((e)=>{
     let x1 = ((e[0] * this.rotation[1] + e[1] * this.rotation[0]) + this.x)
-	let y1 = ((e[1] * this.rotation[1] - e[0] * this.rotation[0]) + this.y)
-	let x2 = ((e[2] * this.rotation[1] + e[3] * this.rotation[0]) + this.x)
-	let y2 = ((e[3] * this.rotation[1] - e[2] * this.rotation[0]) + this.y)
+  let y1 = ((e[1] * this.rotation[1] - e[0] * this.rotation[0]) + this.y)
+  let x2 = ((e[2] * this.rotation[1] + e[3] * this.rotation[0]) + this.x)
+  let y2 = ((e[3] * this.rotation[1] - e[2] * this.rotation[0]) + this.y)
       line(x1,y1,x2,y2)
       circle(this.x,this.y,2)
     })
   }
   
+  AA(rx,ry,vx,vy){
+    let V = this.applyForceInstance(rx,ry,vx,vy)
+    this.vx += V[0]
+    this.vy += V[1]
+    this.r -= V[2]
+  }
+  
   applyForceInstance(rx,ry,vx,vy){
     console.log("applyforce")
     allTempLines.push(new TempForceLine(200,"black",this.x+rx,this.y+ry,this.x+rx+(vx*50),this.y+ry+(vy*50)))
-    let A = vectorFuncs.ShatterComponents(vx,vy,rx,ry)
+    let A = vectorFuncs.ShComp(vx,vy,rx,ry)
     
     let ary = ry + this.y
     let arx = rx + this.x
@@ -98,14 +118,10 @@ class PHOBJ{
     allTempLines.push(new TempForceLine(100,"red",arx,ary,arx+A[1][0]*50,ary+A[1][1]*50))
     allTempLines.push(new TempForceLine(100,"blue",arx,ary,arx+A[0][0]*50,ary+A[0][1]*50))
     allTempLines.push(new TempForceLine(100,"green",vx,vy,rx,ry))
-    this.vx += A[0][0]
-    this.vy += A[0][1]
-    this.r -= A[2][1]/distance(this.x,this.y,rx,ry)
-
-    // console.log("applied force:"+distance(0,0,vx,vy))
-    // console.log("object moomentum now:"+this.r+distance(0,0,this.vx,this.vy))
+  
     
-    // setTimeout(()=>{draw=()=>{}},5)
+    
+    return([A[0][0],A[0][1],A[2][1]/distance(0,0,rx,ry)])
   }
   
   
@@ -160,7 +176,7 @@ function draw() {
     
   } else {
     if(mouseState.held){
-      O1.applyForceInstance(mouseState.pos[0]-mouseState.pos[2],mouseState.pos[1]-mouseState.pos[3],(mouseX-mouseState.pos[0])/50,(mouseY-mouseState.pos[1])/50)
+      O1.AA(mouseState.pos[0]-mouseState.pos[2],mouseState.pos[1]-mouseState.pos[3],(mouseX-mouseState.pos[0])/50,(mouseY-mouseState.pos[1])/50)
       // console.log(mouseState.pos[0]-O1.x,mouseState.pos[1]-O1.y,(mouseX-mouseState.pos[0])/50,(mouseY-mouseState.pos[1])/50)
       mouseState.held = false
     }
@@ -184,29 +200,48 @@ function distance(x,y,x2,y2){
 
 
 class vectorFuncs{
-	static vectorizor(px,py,vx,vy){
-		//this.walls[p.boidy[k]].x1 = ((p.boidyVect[k][0] * p.rotation[1] + p.boidyVect[k][1] * p.rotation[0]) + p.x)
-		//xb+ya,yb-xa
-		return([px*vy+py*vx,py*vy-px*vx])
-	}
-	static INvectorizor(px,py,vx,vy){
-		//xb-ya,yb+xa
-		return([px*vy-py*vx,py*vy+px*vx])
-	}
-	static ShatterComponents(vx,vy,dx,dy){
-		let normalized = this.originVectorNormalize(dx,dy)
-		let invectorized = this.INvectorizor(vx,vy,normalized[0],normalized[1])
+  static vectorizor(px,py,vx,vy){
+    //this.walls[p.boidy[k]].x1 = ((p.boidyVect[k][0] * p.rotation[1] + p.boidyVect[k][1] * p.rotation[0]) + p.x)
+    //xb+ya,yb-xa
+    return([px*vy+py*vx,py*vy-px*vx])
+  }
+  static INvectorizor(px,py,vx,vy){
+    //xb-ya,yb+xa
+    return([px*vy-py*vx,py*vy+px*vx])
+  }
+  static ShatterComponents(vx,vy,dx,dy){
+    let normalized = this.originVectorNormalize(dx,dy)
+    let invectorized = this.INvectorizor(vx,vy,normalized[0],normalized[1])
 
-		let tyaxis = this.vectorizor(normalized[0],normalized[1],1,0)
+    let tyaxis = this.vectorizor(normalized[0],normalized[1],1,0)
 
-		let resultx = [invectorized[1]*normalized[0],invectorized[1]*normalized[1]]
-		let resulty = [invectorized[0]*tyaxis[0],invectorized[0]*tyaxis[1]]
-		return([resultx,resulty,[invectorized[1],invectorized[0]]])
+    let resultx = [invectorized[1]*normalized[0],invectorized[1]*normalized[1]]
+    let resulty = [invectorized[0]*tyaxis[0],invectorized[0]*tyaxis[1]]
+    return([resultx,resulty,[invectorized[1],invectorized[0]]])
 
-	}
+  }
 
-	static originVectorNormalize(vx,vy){
-		let d = Math.sqrt(vx*vx+vy*vy)
-		return([vx/d,vy/d])
-	}
+    static ShComp(vx,vy,dx,dy){
+    let normalized = this.originVectorNormalize(dx,dy)
+
+    let dp = this.dotProduct(vx,vy,normalized[0],normalized[1])
+
+    let n2 = [normalized[1],-normalized[0]]
+
+    let dp2 = this.dotProduct(vx,vy,n2[0],n2[1])
+    let resultx = [normalized[0]*dp,normalized[1]*dp]
+    let resulty = [n2[0]*dp2,n2[1]*dp2]
+
+    return([resultx,resulty,[dp,dp2]])
+
+  }
+
+    static dotProduct(x1,y1,x2,y2){
+    return(x1*x2+y1*y2)
+  }
+  
+  static originVectorNormalize(vx,vy){
+    let d = Math.sqrt(vx*vx+vy*vy)
+    return([vx/d,vy/d])
+  }
 }
