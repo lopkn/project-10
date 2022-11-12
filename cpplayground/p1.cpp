f741213a0adc25bce632abd4e1df9e24a59c6d8e
lopkn <leoyuan06@gmail.com>
2022-11-13 00:02:38 +0800

    feat: learning pointer

--- cpplayground/p1.cpp
+++ cpplayground/p1.cpp
@@ -4,7 +4,8 @@
 #include <string>
 #include <map>
 #include <cmath>
-
+#include <ctime>
+#include <chrono>
 
 void d1(){
   using namespace std;
@@ -58,21 +59,42 @@ float mrand(){
   return((float)rand() / (float)RAND_MAX);
 };
 
+uint64_t timenow() {
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+
 class nur{
 public:
   double act;
   double xpt;
   int actors;
 
+  nur(*brain)[100];
+
+  bool used = false;
+  bool stable = false;
+  double adder = 0;
+
+  nur * linkedNur;
+  int linkedNurNum = -1;
+
   double knowns[20][4];
   int knownCounter = 0;
 
   double multipliers[3] = {0,0,0};
 
-  double process(double inp[3],double expected,int learntimes){
+  double process(double inp[3],double expected,uint64_t learntimes,nur(*n)[100]){
     
+    used = true;
+
+    brain = n;
+
+    double originalError = evaluateAll(multipliers,adder);
+
+
     if(knownCounter < 20){
-      // double a[4] = {inp[0],inp[1],inp[2],expected};
       knowns[knownCounter][0] = inp[0];
       knowns[knownCounter][1] = inp[1];
       knowns[knownCounter][2] = inp[2];
@@ -87,55 +109,60 @@ public:
     };
 
     double error = fabs(out-expected);
-    // int rev = 1;
-    // if(mrand()>0.5){
-    //   rev = -1;
-    // };
 
 
-    double fe = learn(learntimes,error);
+    double fe = learn(learntimes,error,originalError);
 
 
 
-    // for(int i = 0; i < 3; i++){
-    //   multipliers[i] += rev*mrand()*error*0.5;
-    // };
+
     std::cout << "error: "<< fe <<"\n";
-    std::cout << "multipliers: "<<multipliers[0]<<", "<<multipliers[1]<<", "<<multipliers[2]<<" |";
+    std::cout << "multipliers: "<<multipliers[0]<<", "<<multipliers[1]<<", "<<multipliers[2]<<" + "<<adder<<" |";
 
-    return(inp[0]*multipliers[0]+inp[1]*multipliers[1]+inp[2]*multipliers[2]);
+    return(question(inp[0],inp[1],inp[2]));
   };
 
-  double evaluate(double inp[3],double multiplier[3],double expected){
+  double evaluate(double inp[3],double multiplier[3],double ad,double expected){
+    used = true;
     double out = 0;
     for(int i = 0; i < 3; i++){
       out += inp[i]*multiplier[i];
     };
 
+    out += ad;
+
     double error = fabs(out-expected);
     return(error);
   };
 
-  double evaluateAll(double multiplier[3]){
+  double question(double a,double b,double c){
+    return(a*multipliers[0]+b*multipliers[1]+c*multipliers[2]+adder);
+  }
+
+  double evaluateAll(double multiplier[3], double ad){
     double currentError = 0;
     for(int i = 0; i < knownCounter; i++){
       double a[3] = {knowns[i][0],knowns[i][1],knowns[i][2]};
-      currentError += evaluate(a,multiplier,knowns[i][3]);
+      currentError += evaluate(a,multiplier,ad,knowns[i][3]);
     };
     return(currentError);
   };
 
-  double learn(int times, double error){
+  double learn(uint64_t times, double error, double orig){
+
+    used = true;
 
     double temp[3] = {multipliers[0],multipliers[1],multipliers[2]};
 
-    double currentError = evaluateAll(temp);
+    double currentError = evaluateAll(temp,adder);
 
 
     int changes = 0;
 
+    double beforeError = currentError;
+    double BEFOREM[4] = {multipliers[0],multipliers[1],multipliers[2],adder};
 
-    for(int i = 0; i < times; i++){
+    while(times > timenow()){
 
     int rev = 1;
     if(mrand()>0.5){
@@ -143,22 +170,43 @@ public:
     };
 
     double Tmultipliers[3] = {multipliers[0],multipliers[1],multipliers[2]};
+    double tadder = adder;
+    int R = std::floor(mrand()*4);
+    if(R < 3){
+      Tmultipliers[R] += rev*mrand()*error*0.25/knownCounter;
+    } else {
+      tadder += rev*mrand()*error*0.25/knownCounter;
+    };
 
-    int R = std::floor(mrand()*3);
-    // for(int i = 0; i < 3; i++){
-      Tmultipliers[R] += rev*mrand()*error*0.5;
-    // };
-
-    double newError = evaluateAll(Tmultipliers);
+    double newError = evaluateAll(Tmultipliers,tadder);
+    if(stable == false){
     if(newError < currentError){
       multipliers[0] = Tmultipliers[0];
       multipliers[1] = Tmultipliers[1];
       multipliers[2] = Tmultipliers[2];
+      adder = tadder;
       currentError = newError;
       changes += 1;
+      };
+    } else {
+
     };
 
   };
+
+  if(beforeError-currentError < 0.1 || orig-currentError < -0.5){
+    if(currentError > 0.1){
+      int nextNurNum = floor(mrand()*100);
+      if((*brain)[nextNurNum].used == false){
+        (*brain)[nextNurNum].used = true;
+        stable = true;
+        linkedNur = &(*brain)[nextNurNum];
+        linkedNurNum = nextNurNum;
+      };
+    };
+  };
+
+
   std::cout<<"\nchanged: "<<changes<<" times\n";
 
   return(currentError);
@@ -168,48 +216,81 @@ public:
 };
 
 int main(){
-  nur n1;
+
+  nur NEURONS[100];
+
+  int currentNur = 0;
+
+  nur (*NEURONPOINTER)[100] = &NEURONS;
+
   using namespace std;
   while(true){
 
-    cout << "what do you want it to learn? -> \n";
-    string in1;
-    string in2;
-    string in3;
-    string in4;
+    cout << "operation? -> ";
+    string operation;
+    getline(cin, operation);
 
-    getline(cin, in1);
-    getline(cin, in2);
-    getline(cin, in3);
-    getline(cin, in4);
+    if(operation == "l"){
 
-    double ai1 = stod(in1);
-    double ai2 = stod(in2);
-    double ai3 = stod(in3);
-    double ai4 = stod(in4);
+      cout << "what do you want it to learn? -> \n";
+      string in1;
+      string in2;
+      string in3;
+      string in4;
 
-    double aina[3] = {ai1,ai2,ai3};
+      getline(cin, in1);
+      getline(cin, in2);
+      getline(cin, in3);
+      getline(cin, in4);
 
-    // double out = n1.process(aina,ai4);
+      double ai1 = stod(in1);
+      double ai2 = stod(in2);
+      double ai3 = stod(in3);
+      double ai4 = stod(in4);
 
-    // cout <<"got: "<< out << "\n";
-  // };
+      double aina[3] = {ai1,ai2,ai3};
 
-    cout << "attempts?";
-    string attpt;
-    getline(cin, attpt);
 
-    int attp = stoi(attpt);
 
-  // for(int i = 0; i < attp; i++){
-    // double a[3] = {1,0,0};
-    double out = n1.process(aina,ai4,attp);
-    // if(i > attp-5 || i < 3){
-    //   cout << "got: "<<out<<"\n";
-    // };
-    cout << "got: "<<out<<"\n";
-  // };
+      cout << "attempts?";
+      string attpt;
+      getline(cin, attpt);
 
-  }
+      uint64_t attp = stoi(attpt) + timenow();
+      cout << timenow() << "-" << attp;
+      if(stoi(attpt) > 0){
+      double out = NEURONS[currentNur].process(aina,ai4,attp,NEURONPOINTER);
+
+      cout << "got: "<<out<<"\n";
+      }
+
+    } else if(operation == "q"){
+      cout << "what do you want it to answer? -> \n";
+      string in1;
+      string in2;
+      string in3;
+      getline(cin, in1);
+      getline(cin, in2);
+      getline(cin, in3);
+      double ai1 = stod(in1);
+      double ai2 = stod(in2);
+      double ai3 = stod(in3);
+
+      cout << NEURONS[currentNur].question(ai1,ai2,ai3)<<"\n";
+    } else if(operation == "s"){
+
+      string in1;
+      getline(cin, in1);
+      int ai1 = stoi(in1);
+
+      currentNur = ai1;
+
+    } else if(operation == "p"){
+      cout << NEURONS[currentNur].linkedNurNum;
+    } else if(operation == "t"){
+      time_t T = timenow();
+      cout << T;
+    };
+  };
 
 }
