7ad641ace470dc4f836ef3b7f66e0a138a3af1ae
lopkn <leoyuan06@gmail.com>
2023-03-05 23:51:02 +0800

    feat: operators list

--- public/quick/nurtester/sketch3.js
+++ public/quick/nurtester/sketch3.js
@@ -1,5 +1,29 @@
-let Mem = {"working":{}}
+let Mem = {"working":{},"words":{}}
 
+let funcs = {
+	"when seeing a word":(str)=>{
+		if(funcs["is this a yes?"](funcs["do i know what this word means?"](str))){
+			
+		}
+	}
+}
+let opr = [
+  [false,false,false,true],
+  [false,false,true,false],
+  [false,false,true,true],
+  [false,true,true,true],
+  [false,true,false,true],
+  [false,true,true,false],
+  [false,true,false,false],
+  
+  [true,true,true,false],
+  [true,true,false,true],
+  [true,true,false,false],
+  [true,false,false,true],
+  [true,false,false,false],
+  [true,false,true,false],
+  [true,false,true,true]
+]
 // function a(){
 // 	if(mem["current sentence"] !== undefined){
 // 		if(mem["current sentence"].length !== 1){
@@ -12,6 +36,7 @@ let Mem = {"working":{}}
 // 	}
 // }
 
+
 function sentAn(str){
 	let mem = Mem.working
 	mem["current sentence"] = {}
@@ -19,5 +44,9 @@ function sentAn(str){
 	s.sentence = str
 	s.split = str.split(" ")
 	s["word count"] = s.split.length
-	
+
+	s.split.forEach((e)=>{
+		funcs["when seeing a word"](e)
+	})
+
 }
