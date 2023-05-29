e9c0debfbab0b9c81390f7ef0514094109a8a646
lopkn <leoyuan06@gmail.com>
2023-05-29 21:54:48 +0800

    feat: more questions

--- public/quick/hell/sketch.js
+++ public/quick/hell/sketch.js
@@ -362,75 +362,79 @@ manager.newCard("does this work too?","yes",["TEST"])
 
 
 
-manager.newCard("how to test for carboxylic acid (COOH)?","Add Na2CO3. Organic salt is formed and gas bubbles appear",["@chem@"])
-manager.newCard("how to test for alcohol?"," 1. add NA metal (gas bubbles)\n 2. add PCl5 (fumes)\n 3. add acid (fruity smell)",["@chem@"])
-manager.newCard("how to test for ketones?","2,4-DNP. Orange percipitate is formed",["@chem@"])
-manager.newCard("how to test for aldehydes?","tollens reagent. Silver mirror is formed on surface",["@chem@"])
+// manager.newCard("how to test for carboxylic acid (COOH)?","Add Na2CO3. Organic salt is formed and gas bubbles appear",["@chem@"])
+// manager.newCard("how to test for alcohol?"," 1. add NA metal (gas bubbles)\n 2. add PCl5 (fumes)\n 3. add acid (fruity smell)",["@chem@"])
+// manager.newCard("how to test for ketones?","2,4-DNP. Orange percipitate is formed",["@chem@"])
+// manager.newCard("how to test for aldehydes?","tollens reagent. Silver mirror is formed on surface",["@chem@"])
 
 
 
-manager.newCard("trend of solubility of HYDROXIDE and SULFATE down group 2","Hydroxides: more soluable down\nSulfates: less soluable down (small big rule)",["@chem@"])
-manager.newCard("Flame colors: Li, Na, K, Rb, Cs, Cu, Pb\nCa, Sr, Ba","red, yellow, lilac, red, blue, blueGreen!, lame grey\nbrickRead, crimson red, green",["@chem@"])//seperate please
-manager.newCard("Halogens reactivity down the group Increases or decreases?","increases",["@chem@"])
+// manager.newCard("trend of solubility of HYDROXIDE and SULFATE down group 2","Hydroxides: more soluable down\nSulfates: less soluable down (small big rule)",["@chem@"])
+// manager.newCard("Flame colors: Li, Na, K, Rb, Cs, Cu, Pb\nCa, Sr, Ba","red, yellow, lilac, red, blue, blueGreen!, lame grey\nbrickRead, crimson red, green",["@chem@"])//seperate please
+// manager.newCard("Halogens reactivity down the group Increases or decreases?","increases",["@chem@"])
 
 
 
-manager.newCard("PLUS C WHEN INTEGRATING","REMEMBER TO PLUS C",["@math@"])
-manager.newCard("what is log a (x) in terms of log b ()?","log b (x) / log b (a)",["@math@"])
-manager.newCard("definition of segment of circle? whats the area of a segment?","segment = the semicircle\narea = 0.5 * r^2 * sin(angle)")
+// manager.newCard("PLUS C WHEN INTEGRATING","REMEMBER TO PLUS C",["@math@"])
+// manager.newCard("what is log a (x) in terms of log b ()?","log b (x) / log b (a)",["@math@"])
+// manager.newCard("definition of segment of circle? whats the area of a segment?","segment = the semicircle\narea = 0.5 * r^2 * sin(angle)")
 
 
 
 
 
-manager.newCard("pV = nRT; unit of v?","m^3, (remember that T is in kelvin",["@chem@"])
-manager.newCard("steps to write ionic equations?","1. keep all elements that are not aqueous\n2. get charge of each atom\n3. cancel out useless elements",["@chem@"])
-manager.newCard("Mass spectrometer uses ___ field to accelerate\nand ____ field to deflect ions","electric field, magnetic field",["@chem@"])
-manager.newCard("explain first and second abnormally low IE","new orbital, opposite spin in P orbital",["@chem@"])
-manager.newCard("name all chemical bonding structures","Giant metallic, Giant covalent, Giant ionic, Simple molecular",["@chem@"])
-manager.newCard("Trend of IE across Period and Group","Period: Increase of effective nuclear charge\nGroup: Decrease because radius increase",["@chem@"])
-manager.newCard("Cation is + or -?","-; Cats die.",["@chem@"])
-manager.newCard("polarising power means","FOR CATION. increases with higher charge or smaller size.\nMore easily distort the bond",["@chem@"])
-manager.newCard("covalent bonding definition","electrostatic attraction between two nuclei and shared pairs of electrons",["@chem@"])
-manager.newCard("electronegativity definition","ability of an atom to attract bonding electrons",["@chem@"])
-manager.newCard("whats inbetween 'ionic bond' and 'covalent bond'","polar covalent bond",["@chem@"])
-manager.newCard("true or false dipole is from + to -","true",["@chem@"])
-manager.newCard("Molecule shapes from 2 bonds to 6 bonds","linear, trigonal planar, tetrahedral 109.5, bipyramidal 120-90\noctahedral 90",["@chem@"])
-manager.newCard("each pair of lone electrons decreases bond angle by","2-2.5",["@chem@"])
-manager.newCard("Boiling point for metal increase or decrease down a group","decrease",["@chem@"])
-manager.newCard("Hazard vs risk","Hazards have different risks. Same hazard: is acidic, different risks: can die or can burn",["@chem@"])
-manager.newCard("formula for acid rain","2NO + O2 - > 2NO2; H2O + NO2 -> HNO3 + HNO2",["@chem@"])
-manager.newCard("SO2 pollutant is formed in atmosphere by","impurities of burning coal; power station",["@chem@"])
-manager.newCard("CH4 + Cl2 (free radical sub) -> ","CH3Cl + HCl",["@chem@"])
-manager.newCard("Structural isomerism means","same molecular formula \n(chain, position, functional group[ketone <-> aldehydes])",["@chem@"])
-manager.newCard("Geometrical isomerism means","Cis trans. Cis == E, trans == Z",["@chem@"])
-manager.newCard("Alkene + bromine test. Qualitative or quantitative?","qualitative",["@chem@","@bio@"])
-manager.newCard("Major or minor product is determined by","Major: Hydrogen is connected to the carbon with lowest carbocation",["@chem@"])
-manager.newCard("State standard condition","100kPa, 298K, 1 mol",["@chem@"])//?
-manager.newCard("Enthalpy diagram Up or down is exothermic?","down",["@chem@"])//may delete for easy
-manager.newCard("Standard enthalpy change of FORMATION and ATOMIZATION","",["@chem@"])// NO ANSWER YET
-manager.newCard("Note: bond enthalpy is only in gasses form","Note: water has a lower density because the LATTICE is inefficient at packing",["@chem@"])//note
-manager.newCard("Name all intermolecular forces","London forces (includes instantaneous & induced dipoles)\npermanent dipole\nhydrogen bonds",["@chem@"])
-manager.newCard("Cr2O72- oxidation number equation","2x + 7(-2) = -2",["@chem@"])
-manager.newCard("Oxidation number and Electron number trend During OXIDATION","increase, decrease (opposite for reduction)",["@chem@"])
-manager.newCard("Cl2 + NaOH -> (balance and complete)","Cl2 + 2NaOH -> NaCl + NaOCl + H2O",["@chem@"])
-manager.newCard("Metals and Halogens are generally reducing or oxidizing agents?","metal: reducing agent\nhalogen: oxidizing agent",["@chem@"])
-manager.newCard("IONIC HALF EQUATIONS steps","1. Get reactants and product; 2. add Water to balance oxygen\n3. balance H+ from water; 4: balance charge with electrons",["@chem@"])// memorise products
-manager.newCard("Trend of reactivity of group 1","increase down the group, larger size: easier to lose electron",["@chem@"])
-manager.newCard("Metal oxide + water ->","metal hydroxide",["@chem@"])
-manager.newCard("Metal + water ->","metal oxide + H2",["@chem@"])
+// manager.newCard("pV = nRT; unit of v?","m^3, (remember that T is in kelvin",["@chem@"])
+// manager.newCard("steps to write ionic equations?","1. keep all elements that are not aqueous\n2. get charge of each atom\n3. cancel out useless elements",["@chem@"])
+// manager.newCard("Mass spectrometer uses ___ field to accelerate\nand ____ field to deflect ions","electric field, magnetic field",["@chem@"])
+// manager.newCard("explain first and second abnormally low IE","new orbital, opposite spin in P orbital",["@chem@"])
+// manager.newCard("name all chemical bonding structures","Giant metallic, Giant covalent, Giant ionic, Simple molecular",["@chem@"])
+// manager.newCard("Trend of IE across Period and Group","Period: Increase of effective nuclear charge\nGroup: Decrease because radius increase",["@chem@"])
+// manager.newCard("Cation is + or -?","-; Cats die.",["@chem@"])
+// manager.newCard("polarising power means","FOR CATION. increases with higher charge or smaller size.\nMore easily distort the bond",["@chem@"])
+// manager.newCard("covalent bonding definition","electrostatic attraction between two nuclei and shared pairs of electrons",["@chem@"])
+// manager.newCard("electronegativity definition","ability of an atom to attract bonding electrons",["@chem@"])
+// manager.newCard("whats inbetween 'ionic bond' and 'covalent bond'","polar covalent bond",["@chem@"])
+// manager.newCard("true or false dipole is from + to -","true",["@chem@"])
+// manager.newCard("Molecule shapes from 2 bonds to 6 bonds","linear, trigonal planar, tetrahedral 109.5, bipyramidal 120-90\noctahedral 90",["@chem@"])
+// manager.newCard("each pair of lone electrons decreases bond angle by","2-2.5",["@chem@"])
+// manager.newCard("Boiling point for metal increase or decrease down a group","decrease",["@chem@"])
+// manager.newCard("Hazard vs risk","Hazards have different risks. Same hazard: is acidic, different risks: can die or can burn",["@chem@"])
+// manager.newCard("formula for acid rain","2NO + O2 - > 2NO2; H2O + NO2 -> HNO3 + HNO2",["@chem@"])
+// manager.newCard("SO2 pollutant is formed in atmosphere by","impurities of burning coal; power station",["@chem@"])
+// manager.newCard("CH4 + Cl2 (free radical sub) -> ","CH3Cl + HCl",["@chem@"])
+// manager.newCard("Structural isomerism means","same molecular formula \n(chain, position, functional group[ketone <-> aldehydes])",["@chem@"])
+// manager.newCard("Geometrical isomerism means","Cis trans. Cis == E, trans == Z",["@chem@"])
+// manager.newCard("Alkene + bromine test. Qualitative or quantitative?","qualitative",["@chem@","@bio@"])
+// manager.newCard("Major or minor product is determined by","Major: Hydrogen is connected to the carbon with lowest carbocation",["@chem@"])
+// manager.newCard("State standard condition","100kPa, 298K, 1 mol",["@chem@"])//?
+// manager.newCard("Enthalpy diagram Up or down is exothermic?","down",["@chem@"])//may delete for easy
+// manager.newCard("Standard enthalpy change of FORMATION and ATOMIZATION","",["@chem@"])// NO ANSWER YET
+// manager.newCard("Note: bond enthalpy is only in gasses form","Note: water has a lower density because the LATTICE is inefficient at packing",["@chem@"])//note
+// manager.newCard("Name all intermolecular forces","London forces (includes instantaneous & induced dipoles)\npermanent dipole\nhydrogen bonds",["@chem@"])
+// manager.newCard("Cr2O72- oxidation number equation","2x + 7(-2) = -2",["@chem@"])
+// manager.newCard("Oxidation number and Electron number trend During OXIDATION","increase, decrease (opposite for reduction)",["@chem@"])
+// manager.newCard("Cl2 + NaOH -> (balance and complete)","Cl2 + 2NaOH -> NaCl + NaOCl + H2O",["@chem@"])
+// manager.newCard("Metals and Halogens are generally reducing or oxidizing agents?","metal: reducing agent\nhalogen: oxidizing agent",["@chem@"])
+// manager.newCard("IONIC HALF EQUATIONS steps","1. Get reactants and product; 2. add Water to balance oxygen\n3. balance H+ from water; 4: balance charge with electrons",["@chem@"])// memorise products
+// manager.newCard("Trend of reactivity of group 1","increase down the group, larger size: easier to lose electron",["@chem@"])
+// manager.newCard("Metal oxide + water ->","metal hydroxide",["@chem@"])
+// manager.newCard("Metal + water ->","metal oxide + H2",["@chem@"])
 
 
 
-manager.newCard("Group 1(cept lithium) and 2 thermal stability trend of NO3","",["@chem@"])//
+// manager.newCard("Group 1(cept lithium) and 2 thermal stability trend of NO3","",["@chem@"])//
+
+
+
+
+
+manager.newCard("what is standard enthalpy change of lattice","energy forming 1 mol of solid ionic compound",["@chem@"])
+manager.newCard("what is first electron affinity energy","the energy of gaseous ion formed by adding 1 mol electrons",["@chem@"])
+manager.newCard("whats the outter layer of virus","capsid",["@bio@"])
 manager.newCard("","",["@chem@"])
 manager.newCard("","",["@chem@"])
 manager.newCard("","",["@chem@"])
 manager.newCard("","",["@chem@"])
-manager.newCard("","",["@chem@"])
-manager.newCard("","",["@chem@"])
-manager.newCard("","",["@chem@"])
-
 
 
 
