17910469aba16dc8d1b1ea30610e7d14c78d46ef
lopkn <leoyuan06@gmail.com>
2023-07-30 22:40:25 -0700

    chore: linker update

--- public/modules/myLinker.js
+++ public/modules/myLinker.js
@@ -1,16 +1,19 @@
-class linker{
+class links{
 
-	static actDict = {}	
-	static dict = {}
+	static linkTo = {}	
+	static linkFrom = {}
 
 	static counter = 0
 
+
+
+}
+
+class linker{
 	constructor(l1,l2){
-		
+			this.id = links.counter
+			links.counter++
+			this.l1 = l1
+			this.l2 = l2
 	}
-
-
-
-
-
 }
