b68b1a5f8099712a0ef459adc670a3ab6b925a35
lopkn <leoyuan06@gmail.com>
2023-09-20 20:11:54 -0700

    chore: stuff

--- public/quick/skript/script2.cpp
+++ public/quick/skript/script2.cpp
@@ -10,24 +10,29 @@ int main()
 
   const std::string stupids[31] = {
   "idiotic ",
-  "moronic ","bloody unintelligent ","catastrophicalaly brain dead ",//4
+  "moronic ","bloody unintelligent ","brain dead ",//4
   "vapid ","pig headed ","cretinous ","naive ","senseless ",//9
   // "homosexual ","gay",
   "degenerate ","aberrant ","delerious ","apeish ","puerile ","lame and dopey ", "uneducated ",//16
   "asinine ","unintellectual ","fallacious ","daft ","slow ", "obtuse ",//22
   "feeble ", "uneducated ","invalid ","inconsequential ","frivolous ","protohominid ","negligable ",//29
-  "rancid","primal brained"//31
+  "rancid ","primal brained "//31
 };
 
-const std::string adverbs[21]={
+const std::string adverbs[22]={
   "trivially ","horrendously ","heinously ","erroneously ","notably ","exceptionally ",
-  "abnormally ","abhorrently ","atrociously ","direly "," hideously ","dreadfully ","awfully ","harrowingly ",
-  "unbelievably ","improbably ","implausibly ","disturbingly ","excrutiatingly ", "catastrophicalaly ","absurdly "
+  "abnormally ","abhorrently ","atrociously ","direly ","hideously ","dreadfully ","awfully ","harrowingly ",
+  "unbelievably ","improbably ","implausibly ","disturbingly ","excrutiatingly ", "catastrophically ","absurdly ",
+
+  // 20sep :(
+  "incomprehensibly "
 };
 
-  a = rand() % 30;
-  int b = rand()%20;
+  for(int i = 0; i < 20; i++){
+  a = rand() % 31;
+  int b = rand()%22;
   std::cout << adverbs[b] + stupids[a] + " " << std::endl;
+  }
   return 0;
 }
 
