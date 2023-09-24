e7059854a068aef7402bb89f853675613b2e921c
lopkn <leoyuan06@gmail.com>
2023-09-24 01:25:48 -0700

    chore: stuff

--- CProject/keyReader/test.cpp
+++ CProject/keyReader/test.cpp
@@ -1373,6 +1373,38 @@ void AK(std::string str){
 	system(str2.c_str());
 }
 
+std::string AKchooser()
+{
+  int a;
+  srand (time(NULL));
+
+  const std::string stupids[31] = {
+  "idiotic ",
+  "moronic ","bloody unintelligent ","brain dead ",//4
+  "vapid ","pig headed ","cretinous ","naive ","senseless ",//9
+  // "homosexual ","gay",
+  "degenerate ","aberrant ","delerious ","apeish ","puerile ","lame and dopey ", "uneducated ",//16
+  "asinine ","unintellectual ","fallacious ","daft ","slow ", "obtuse ",//22
+  "feeble ", "uneducated ","invalid ","inconsequential ","frivolous ","protohominid ","negligable ",//29
+  "rancid ","primal brained "//31
+};
+
+const std::string adverbs[22]={
+  "trivially ","horrendously ","heinously ","erroneously ","notably ","exceptionally ",
+  "abnormally ","abhorrently ","atrociously ","direly ","hideously ","dreadfully ","awfully ","harrowingly ",
+  "unbelievably ","improbably ","implausibly ","disturbingly ","excrutiatingly ", "catastrophically ","absurdly ",
+
+  // 20sep :(
+  "incomprehensibly "
+};
+
+
+  a = rand() % 31;
+  int b = rand()%22;
+  std::cout << adverbs[b] + stupids[a] + " " << std::endl;
+  
+  return (adverbs[b] + stupids[a] + " ");
+}
 
 //matrix correlation
 //Eye tracker aim bot
