7af296368aac99d490cdf9c5f9f550feff12efc6
lopkn <leoyuan06@gmail.com>
2023-10-20 15:26:20 -0700

    chore: argaug progress

--- public/quick/argaug/sketch.js
+++ public/quick/argaug/sketch.js
@@ -41,14 +41,29 @@ ctx.fillRect(0,0,Width,Height)
 setTimeout(()=>{ctx.clearRect(0,0,Width,Height)},200)
 
 
+var drag = {"sx":0,"sy":0,"x":0,"y":0,"on":false}
 
+setInterval(()=>{mainInterval()},30)
 
-{ 
+function mainInterval(){
+	ctx.clearRect(0,0,Width,Height)
 
-	let elms = classQuery("test")
-	for( let i = 0 ; i < elms.length; i++){
-		let el = elms[i]
-		el.addEventListener('contextmenu',(e)=>{
+	if(drag.on){
+		console.log("hi")
+		ctx.strokeStyle="#FFFFFF";
+		ctx.lineWidth = 3;
+
+		ctx.beginPath()
+		ctx.moveTo(drag.sx,drag.sy)
+		ctx.lineTo(drag.x,drag.y)
+		ctx.stroke()
+
+	}
+
+}
+
+function setDefaultStatementBx(el){
+	el.addEventListener('contextmenu',(e)=>{
 			e.preventDefault();
 			console.log("Elm rc: "+el+" at X: "+e.clientX+" Y: "+e.clientY)
 			// el.contentEditable = false;
@@ -67,10 +82,101 @@ setTimeout(()=>{ctx.clearRect(0,0,Width,Height)},200)
 		el.addEventListener('dragstart',(e)=>{
 			console.log("Elm drg: "+el+" at X: "+e.clientX+" Y: "+e.clientY)
 			e.dataTransfer.setDragImage(new Image(), 0, 0);
+			drag = {"sx":e.clientX,"sy":e.clientY,"x":e.clientX,"y":e.clientY,"on":true}
+		})
+		el.addEventListener('drag',(e)=>{
+			drag.x = e.clientX
+			drag.y = e.clientY
 		})
 		el.addEventListener('dragend',(e)=>{
 			console.log("Elm drgend: "+el+" at X: "+e.clientX+" Y: "+e.clientY)
+			drag.on = false;
 		})
+
+		el.contentEditable = false;
+		el.style.userSelect = "none"
+		el.draggable = true
+}
+
+{ 
+
+	let elms = classQuery("statementBx")
+	for( let i = 0 ; i < elms.length; i++){
+
+		let el = elms[i]
+		setDefaultStatementBx(el)
 	}
 
 }
+
+
+class storage{
+	static main = {"0":{
+		"title":"Welcome to lopknA65's domain",
+		"description":"Feel free to explore",
+		"options":[{"optTitle":"hi","tags":[0]},{"optTitle":"hello","tags":[0]}]
+	}}
+}
+
+
+class Arg {
+	static path = []
+	static currentTag = 0
+	static currentTitle = ""
+
+	static loadTag(tag){
+
+		
+		this.path.push(tag)
+		let pathStr = " <  "
+		this.path.forEach((e,i)=>{
+			if(i%2==0){
+				pathStr += "["+e+"]"
+			} else {
+				pathStr += "{"+e+"}"				
+			}
+		})
+		document.getElementById("path").innerHTML = pathStr
+		
+		let content = storage.main[tag]
+
+		document.getElementById("title").innerHTML = content.title
+		//description
+
+		let elm = document.getElementById("container1")
+		let children = elm.children
+		for(let i = children.length-1; i > -1; i--){
+			if(children[i].id !== "action"){
+				elm.removeChild(children[i])
+			}
+		}
+
+
+		content.options.forEach((option,i)=>{
+			let el = document.createElement("div")
+			el.innerHTML = option.optTitle
+			el.classList.add("statementBx")
+			setDefaultStatementBx(el)
+			document.getElementById("container1").insertBefore(el,document.getElementById("container1").firstChild)
+		})
+
+		
+
+	}
+}
+
+Arg.loadTag(0)
+
+
+/// Tag ID, tag title, tag description, tag options (link)
+
+let example = {
+	"title":"1+1",
+	"description":"this means one apple and another apple is two apples combined",
+	"options":[{"optTitle":"yes this is true","tags":[16,17]},{"optTitle":"no","tags":[73,52]}]
+}
+
+
+/// [41]{5} -> [58]{3} -> [29]<17>
+
+///guideline: ychain, understand -> sound -> valid
