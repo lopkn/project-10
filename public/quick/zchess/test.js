b8948aeb5b0002ad422f4f0ad347dcd266a60c91
lopkn <leoyuan06@gmail.com>
2023-11-15 21:45:24 -0800

    chore: some stupid stuff

--- public/quick/zchess/test.js
+++ public/quick/zchess/test.js
@@ -5,10 +5,28 @@ class WorkletProcessor extends AudioWorkletProcessor {
     // Do something with the data, e.g. convert it to WAV
     out ++
     if(out%20==0){
-      console.log(inputs[0][0][0])
+      // console.log(inputs[0][0][0])
+      console.log(inputs)
     }
     return true;
   }
 }
 
 registerProcessor("worklet-processor", WorkletProcessor);
+
+
+
+// let workProcessor;
+// let audioContext;
+// navigator.mediaDevices.getUserMedia({ audio: true }).then((stream)=>{
+//     audioContext = new AudioContext();
+//     audioContext.createMediaStreamSource(stream)
+//     workProcessor = audioContext.audioWorklet.addModule("./test.js");
+//     console.log("Audio context successfully initialized.");
+    
+// })
+
+
+
+
+/// wait then manually do  audioWorkletNode = new AudioWorkletNode(audioContext, 'worklet-processor')
