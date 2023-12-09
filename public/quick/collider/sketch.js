20f55dfe247b0df07ca970aa2a8a543baef8a0df
lopkn <leoyuan06@gmail.com>
2023-12-08 18:01:16 -0800

    feat: collider 2d

--- public/quick/collider/sketch.js
+++ public/quick/collider/sketch.js
@@ -5,20 +5,42 @@ function setup() {
 let balls = []
 let scl=5
 
+let down = false
+
 function draw() {
-  background(220);
+  background(0);
  
   fill(255)
   // if(Math.random()<0.01){
   //   console.log(collBall2D(balls[0],balls[1]).ans)
   // }
-  advance(1)
-  if(collBall2D(balls[0],balls[1]).ans < 1){
-    fill(10,10,10)
+  let c = collBall2D(balls[0],balls[1])
+  if(c.ans < 1){
+    fill(0,100,0)
   }
+  let DDT = 1
+  let breaker = 0
+  let COL = collideDT(balls,DDT*0.01)
+  while(COL.result == "collide"&&breaker < 10 && DDT > 0){
+    breaker++
+    advance(COL.info.ans)
+    momentumTransfer2D(COL.info.b1,COL.info.b2)
+    DDT -= COL.info.ans
+    COL = collideDT(balls,DDT*0.01)
+  } 
+  if(breaker > 10){
+  console.log(breaker)
+  }
+  advance(DDT)
   balls.forEach((e,i)=>{
     circle(e.x*scl,e.y*scl,e.r*scl*2)
   })
+  if(keyIsDown(32)){
+    if(down == false){
+      balls.push(new ball(mouseX/scl,mouseY/scl,5,0,0,true))
+      down = true
+    }
+  } else {down =false}
 }
 
 function advance(dt){
@@ -52,19 +74,21 @@ function collideDT(objs, DT){
 }
 
 class ball{
-	constructor(x,y,r=1,vx=0,vy=0,mouseControl=false){
+	constructor(x,y,r=1,vx=0,vy=0,mouseControl=false,mass = 1){
 		this.x = x
 		this.y = y
 		this.r = r
 		this.vy = vy
 		this.vx = vx
+        this.m = mass
       this.mouseControl = mouseControl
 	}
   
     update1(dt=1){
       this.x += this.vx*dt
       this.y += this.vy*dt
-      if(this.mouseControl && keyIsDown(32)){
+      // if(this.mouseControl && keyIsDown(32)){
+      if(this.mouseControl && mouseIsPressed){
       this.vx -= (this.x-mouseX/scl)*0.5
       this.vy -= (this.y-mouseY/scl)*0.5
       }
@@ -106,14 +130,58 @@ function collBall2D(b1,b2){
         ans = answers[0]
       }
     }
-  if(ans < 0){ans = Infinity}
+  if(ans <= 0){ans = Infinity}
   
-    if(isNaN(ans)){
-      console.log([b1,b2])
-    }
+
   
 	return({"ans":ans,"answers":answers,"b1":b1,"b2":b2})
 
 }
+
+function momentumTransfer2D(b1,b2){
+  let vb12x = b2.x-b1.x
+  let vb12y = b2.y-b1.y //differential vector
+  
+  let normVB12 = normalize(vb12x,vb12y)
+  
+  let dotHx1 = dot(normVB12[0],normVB12[1],b1.vx,b1.vy)
+  let dotHx2 = dot(normVB12[0],normVB12[1],b2.vx,b2.vy)
+  
+  let Vaverage = ( dotHx1 + dotHx2 )/ 2
+  let MT1 = (dotHx1-Vaverage) * b1.m
+  
+  b2.vx += normVB12[0] * MT1 / b2.m
+  // console.log(MT1,dotHx1,normVB12,vb12x,vb12y)
+  b2.vy += normVB12[1] * MT1 / b2.m
+  
+  b1.vx -= normVB12[0] * MT1 / b1.m
+  b1.vy -= normVB12[1] * MT1 / b1.m
+}
+
+
+
+
+function dot(x,y,x2,y2){
+  return(x*x2+y2*y)
+}
+
+function normalize(x,y){
+  let d = Math.sqrt(x*x+y*y)
+  return([x/d,y/d])
+}
+
+
+function distance(x1,y1,x2,y2) {
+	let a = x2-x1
+	let b = y2-y1
+  return(Math.sqrt(a*a+b*b))
+}
+
+
+
+
+
+
+
 	
 
