628c6f0f0f79d194dd4cbcbffe11eb947e29a031
lopkn <leoyuan06@gmail.com>
2023-12-09 19:21:02 -0800

    feat: collider debugger

--- public/quick/collider/sketch.js
+++ public/quick/collider/sketch.js
@@ -21,12 +21,15 @@ function draw() {
   let DDT = 1
   let breaker = 0
   let COL = collideDT(balls,DDT*0.01)
-  while(COL.result == "collide"&&breaker < 10 && DDT > 0){
+  while(COL.result == "collide"&&breaker < 10000 && DDT > 0){
     breaker++
     advance(COL.info.ans)
     momentumTransfer2D(COL.info.b1,COL.info.b2)
     DDT -= COL.info.ans
     COL = collideDT(balls,DDT*0.01)
+    if(breaker == 99){
+      console.log(dist(COL.info.b1.vx,COL.info.b1.vy,0,0))
+    }
   } 
   if(breaker > 10){
   console.log(breaker)
@@ -52,20 +55,20 @@ function advance(dt){
 
 
 function collideDT(objs, DT){
-	let collideTime = Infinity;
-	let collideInfo = "none";
-	for(let i = 0; i < objs.length; i++){
-		for(let j = i; j < objs.length; j++){
-			//quick check of collision
-			//find collision time
+  let collideTime = Infinity;
+  let collideInfo = "none";
+  for(let i = 0; i < objs.length; i++){
+    for(let j = i; j < objs.length; j++){
+      //quick check of collision
+      //find collision time
             let col = collBall2D(objs[i],objs[j])
             if(col.ans < 1 && col.ans < collideTime){
               //if collide time is smaller, make it the new collision info & time
               collideTime = col.ans
               collideInfo = col
             }
-		}
-	}
+    }
+  }
   if(collideTime > DT){//might check for equality error here later
     return({"result":"advance","time":DT})
   } else {
@@ -74,15 +77,15 @@ function collideDT(objs, DT){
 }
 
 class ball{
-	constructor(x,y,r=1,vx=0,vy=0,mouseControl=false,mass = 1){
-		this.x = x
-		this.y = y
-		this.r = r
-		this.vy = vy
-		this.vx = vx
+  constructor(x,y,r=1,vx=0,vy=0,mouseControl=false,mass = 1){
+    this.x = x
+    this.y = y
+    this.r = r
+    this.vy = vy
+    this.vx = vx
         this.m = mass
       this.mouseControl = mouseControl
-	}
+  }
   
     update1(dt=1){
       this.x += this.vx*dt
@@ -99,24 +102,24 @@ let bl1 = new ball(10,1000000,100,6,-350000)
 let bl2 = new ball(10,110,50,27,35)
 
 balls.push(new ball(0,0,5,1))
-balls.push(new ball(0,2,5,1,0,true))
+balls.push(new ball(0,2,5,1,0))
 
 function collBall2D(b1,b2){
 
-	let b = (b2.vx-b1.vx)
-	let c = b2.x
-	let d = b1.x
-	let e = (b2.vy-b1.vy)
-	let f = b2.y
-	let g = b1.y
+  let b = (b2.vx-b1.vx)
+  let c = b2.x
+  let d = b1.x
+  let e = (b2.vy-b1.vy)
+  let f = b2.y
+  let g = b1.y
 
-	let answers = [NaN,NaN]
-	// let answers = [b,c,d,e,f,g]
+  let answers = [NaN,NaN]
+  // let answers = [b,c,d,e,f,g]
 
-	// t1 = (-2*b*c+2*b*d-2*e*f+2*e*g+-2*Math.sqrt(-b*b*f*f+2*e*b*c*f+2*b*b*f*g-e*e*c*c-e*e*d*d+2*e*e*c*d-b*b*g*g-2*e*b*c*g+2*e*b*d*g)) / 2(b*b+e*e)
+  // t1 = (-2*b*c+2*b*d-2*e*f+2*e*g+-2*Math.sqrt(-b*b*f*f+2*e*b*c*f+2*b*b*f*g-e*e*c*c-e*e*d*d+2*e*e*c*d-b*b*g*g-2*e*b*c*g+2*e*b*d*g)) / 2(b*b+e*e)
 
-	try{answers[0] = (-2*b*c+2*b*d-2*e*f+2*e*g+Math.sqrt(Math.pow(2*b*c-2*b*d+2*e*f-2*e*g,2)-4*(b*b+e*e)*(c*c-2*c*d+d*d-2*f*g+f*f+g*g-Math.pow(b1.r+b2.r,2)))) / (2*(b*b+e*e))}catch{}
-	try{answers[1] = (-2*b*c+2*b*d-2*e*f+2*e*g-Math.sqrt(Math.pow(2*b*c-2*b*d+2*e*f-2*e*g,2)-4*(b*b+e*e)*(c*c-2*c*d+d*d-2*f*g+f*f+g*g-Math.pow(b1.r+b2.r,2)))) / (2*(b*b+e*e))}catch{}
+  try{answers[0] = (-2*b*c+2*b*d-2*e*f+2*e*g+Math.sqrt(Math.pow(2*b*c-2*b*d+2*e*f-2*e*g,2)-4*(b*b+e*e)*(c*c-2*c*d+d*d-2*f*g+f*f+g*g-Math.pow(b1.r+b2.r,2)))) / (2*(b*b+e*e))}catch{}
+  try{answers[1] = (-2*b*c+2*b*d-2*e*f+2*e*g-Math.sqrt(Math.pow(2*b*c-2*b*d+2*e*f-2*e*g,2)-4*(b*b+e*e)*(c*c-2*c*d+d*d-2*f*g+f*f+g*g-Math.pow(b1.r+b2.r,2)))) / (2*(b*b+e*e))}catch{}
 
     let ans = answers[0]>answers[1]?answers[1]:answers[0]
     if(isNaN(answers[0])){
@@ -134,7 +137,7 @@ function collBall2D(b1,b2){
   
 
   
-	return({"ans":ans,"answers":answers,"b1":b1,"b2":b2})
+  return({"ans":ans,"answers":answers,"b1":b1,"b2":b2})
 
 }
 
@@ -148,7 +151,7 @@ function momentumTransfer2D(b1,b2){
   let dotHx2 = dot(normVB12[0],normVB12[1],b2.vx,b2.vy)
   
   let Vaverage = ( dotHx1 + dotHx2 )/ 2
-  let MT1 = (dotHx1-Vaverage) * b1.m
+  let MT1 = (dotHx1-Vaverage*0) * b1.m
   
   b2.vx += normVB12[0] * MT1 / b2.m
   // console.log(MT1,dotHx1,normVB12,vb12x,vb12y)
@@ -172,8 +175,8 @@ function normalize(x,y){
 
 
 function distance(x1,y1,x2,y2) {
-	let a = x2-x1
-	let b = y2-y1
+  let a = x2-x1
+  let b = y2-y1
   return(Math.sqrt(a*a+b*b))
 }
 
@@ -183,5 +186,5 @@ function distance(x1,y1,x2,y2) {
 
 
 
-	
+  
 
