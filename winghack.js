f7befdb08d15599b370300d0ee2b4210b38cf2b5
lopkn <leoyuan06@gmail.com>
2023-12-25 18:36:55 -0800

    feat: actual aimbot

--- winghack.js
+++ winghack.js
@@ -5,6 +5,14 @@ canvas.style.zIndex = 5000
 canvas.style.width = Math.floor(window.innerWidth)+"px"
 canvas.style.height = Math.floor(window.innerHeight)+"px"
 canvas.width = window.innerWidth; canvas.height = window.innerHeight
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  --mouseDown;
+}
+let priorityTargetMode = false
 let ctx = canvas.getContext("2d")
 canvas.style.pointerEvents = "none"
 let highlights = {}
@@ -13,6 +21,12 @@ let lock = false
 let autoFire = false
 let pla = -1
 let zoom = 1
+let last = {}
+let closest = -1;
+let aimedAt = -1;
+
+let adjust = 20
+
 let main = setInterval(()=>{
     ctx.clearRect(0,0,canvas.width,canvas.height)
 
@@ -25,10 +39,12 @@ let main = setInterval(()=>{
     }
 
     let objk = Object.keys(window.B)
-    let closest = -1
+    closest = -1
     let distclosest = Infinity
 objk.forEach((a,i)=>{
   let e = window.B[a]
+       if(e.inGame == false){return}
+
   // if(i == 0){console.log(e.x,e.y)}
     ctx.fillStyle = "red"
     if(highlights[a]){ctx.fillStyle="green"}
@@ -42,18 +58,36 @@ objk.forEach((a,i)=>{
       }
     }
 
+    if(last[a] == undefined){
+      last[a] = {"lx":t[0],"ly":t[1],"vy":0,"vx":0}
+    } else {
+      last[a] = {"vx":t[0]-last[a].lx,"vy":t[1]-last[a].ly,"lx":t[0],"ly":t[1]}
+    }
     
     ctx.fillRect(t[0]-7.5*zoom-lx,t[1]-7.5*zoom-ly,15*zoom,15*zoom)
 })
+  if(aimedAt !== -1){
+    let t = tran(window.B[aimedAt].x,window.B[aimedAt].y)
+    ctx.fillStyle = "rgba(0,0,0)"
+    ctx.fillRect(t[0]-5-lx,t[1]-5-ly,10,10)
+  }
   if(closest !== -1){
     let t = tran(window.B[closest].x,window.B[closest].y)
     ctx.fillStyle = "rgba(255,0,255,0.5)"
     ctx.fillRect(t[0]-10-lx,t[1]-10-ly,20,20)
     if(lock){
       ctx.beginPath()
+      ctx.lineWidth = 1
       ctx.strokeStyle = "#000000"
       ctx.moveTo(canvas.width/2,canvas.height/2)
       ctx.lineTo(canvas.width/2+3*(t[0]-lx-(canvas.width/2)),canvas.height/2+3*(t[1]-ly-(canvas.height/2)))
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.lineWidth = 2
+      ctx.strokeStyle = "#00FF00"
+      ctx.moveTo(t[0]-lx,t[1]-ly)
+      ctx.lineTo(last[closest].vx*adjust  + t[0]-lx,last[closest].vy*adjust  +t[1]-ly)
       ctx.stroke()
 
       let tranPo = [3*(t[0]-lx-(canvas.width/2)),3*(t[1]-ly-(canvas.height/2))]
@@ -88,9 +122,9 @@ function distance(x1,y1,x2,y2) {
 let mouseX = 0
 let mouseY = 0
 onmousemove = (e)=>{mouseX = (e.clientX); mouseY = (e.clientY)}
+let downs = {}
 document.addEventListener("keydown",(e)=>{
   let key = e.key
-  
   if(key == "q"){
         highlights = {}
      console.log("ho?")
@@ -141,8 +175,22 @@ document.addEventListener("keydown",(e)=>{
   if(key == "]"){
     autoFire=!autoFire
   }
+  if(key == "z"){
+    priorityTargetMode = !priorityTargetMode
+  }
+  if(key == "x" && downs[key] == false){
+    window.A.sendDirection = ()=>{}
+    window.A.sendInput = ()=>{}
+  }
+  downs[e.key] = true
 })
-
+document.addEventListener("keyup",(e)=>{
+  if(e.key == "x"){
+    window.A.sendDirection = window.SD
+    window.A.sendInput = window.SI
+  }
+  downs[e.key] = false
+})
 
 function resize(){
 canvas.width = window.innerWidth; canvas.height = window.innerHeight
@@ -152,20 +200,55 @@ function ang(x,y){
     if(a < 0){a = 2*Math.PI+a}
     return(a)
 }
+
+function getAng(p){
+  return(ang(window.B[p].x-window.B[pla].x,-window.B[p].y+window.B[pla].y))
+}
+
+
+let minDist = 2000
+let shootThreshold = 0.1
+let lead = 40
 function autoF(c){
+  aimedAt = -1
   let tp = [window.B[c].x-window.B[pla].x,window.B[c].y-window.B[pla].y]
   let aEnemy = ang(tp[0],-tp[1])
-  let aMe = ang(mouseX-canvas.width/2,mouseY-canvas.height/2)
+  let aMe = ang(mouseX-canvas.width/2,-(mouseY-canvas.height/2))
   let aGame = window.B[pla].angle%(2*Math.PI)
   if(aGame < 0){
     aGame = 2*Math.PI+aGame
   }
-  // aGame*=2
+
+  let closestAng = Infinity
+  Object.keys(window.B).forEach((a)=>{
+    let e = window.B[a]
+    if(!e.inGame){return}
+    if(a ==pla || distP(pla,a) > minDist){return}
+    let aEnemy2 = ang(window.B[a].x-window.B[pla].x,-window.B[a].y+window.B[pla].y)
+    let tang = Math.abs(aMe-aEnemy2)
+    if(tang < closestAng){closestAng = tang;aimedAt = a}
+  })
+
   // console.log(tp[0])
-  if(Math.abs(aGame-aEnemy) < 0.1){
+  if(downs["x"] == true){
+    c = mouseDown?(aimedAt == -1?c:aimedAt):c
+    let leadingAngle = ang(window.B[c].x+last[c].vx*lead-window.B[pla].x,-(window.B[c].y+last[c].vy*lead-window.B[pla].y))
+      window.U.angle = Math.PI-leadingAngle
+      console.log(window.U.angle)
+      window.SI()
+      window.SD()
+    }
+  if( ((Math.abs(aGame-aEnemy) < shootThreshold && distP(pla,closest) < minDist)|| closestAng < shootThreshold )&&downs["x"]){
     window.A?.sendShooting(1)
-  } else if(Math.abs(aGame-aEnemy) >0.3){
+  } else if(Math.abs(aGame-aEnemy) >0.3 && !mouseDown){
     window.A?.sendShooting(0)
   }
 }
 
+function distP(a,b){
+  return(distance(window.B[a].x,window.B[a].y,window.B[b].x,window.B[b].y))
+}
+
+
+window.SD = window.A.sendDirection
+window.SI = window.A.sendInput
