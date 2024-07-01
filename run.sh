c348cd30ab189e9cd8b0974b22a69cb73d505599
lopkn <leoyuan06@gmail.com>
2024-06-30 22:51:12 -0700

    test: debugging

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
+#!/bin/sh
 echo "using run.sh";
+getconf ARG_MAX
+date
+
+
+
 
 node -e "$(cat server.js)" -i 1>public/debug/errorlog3.txt 2>&1;
+date
+echo "server crashed, reason:";
+echo "$(cat public/debug/errorlog3.txt)"
 echo "server crash 1, restarting in 5 seconds";
+
+sleep 5;
+node -e "$(cat server.js)"
+echo "server crash 1.5, restarting in 5 seconds";
 sleep 5;
 node server.js;
 echo "server crash 2, restarting in 10 seconds";
