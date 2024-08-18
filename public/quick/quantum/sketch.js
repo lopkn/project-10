742c6f25d93e66e36ffc4e9d8ce60071d9c223d3
lopkn <leoyuan06@gmail.com>
2024-08-17 23:42:56 -0700

    chore: quantum results

--- public/quick/quantum/sketch.js
+++ public/quick/quantum/sketch.js
@@ -24,16 +24,23 @@ socket.emit("JOINGAME",GAMESESSION)
 var ID = 0
 socket.on("acknowledge G10.8",(e)=>{ID = e; console.log("joined as "+ID);room = prompt("room")
   socket.emit("room",{"name":room})})
-socket.on("return",(e)=>{console.log(e)})
+socket.on("return",(e)=>{console.log(e)
+
+  document.getElementById("results").innerText = e
+
+})
 
 let b0 = document.createElement("button")
 let b1 = document.createElement("button")
+let res = document.createElement("div")
 
 b1.innerHTML = "send 1"
 b0.innerHTML = "send 0"
+res.innerText = "results appear here"
 
 document.body.appendChild(b1)
 document.body.appendChild(b0)
+document.body.appendChild(res)
 
 b1.onclick = ()=>{socket.emit("measure",{"room":room,"input":1})}
 b0.onclick = ()=>{socket.emit("measure",{"room":room,"input":0})}
