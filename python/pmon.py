e4db41a6e9b79b72b0636af45bb02af50574039e
lopkn <leoyuan06@gmail.com>
2024-10-09 20:10:57 -0700

    chore: plog

--- python/pmon.py
+++ python/pmon.py
@@ -6,55 +6,55 @@ from sys import platform
 import tkinter as tk
 # import threading
 
-# root = tk.Tk()
+root = tk.Tk()
 
-# if platform == "linux" or platform == "linux2":
-#     root.overrideredirect(True)
-#     root.wait_visibility(root)
-#     root.wm_attributes("-alpha", 0.5)
-# elif platform == "darwin":
-#     root.overrideredirect(True)
-#     # Make the root window always on top
-#     root.wm_attributes("-topmost", True)
-#     # Turn off the window shadow
-#     root.wm_attributes("-transparent", True)
-#     # Set the root window background color to a transparent color
-#     root.config(bg='systemTransparent')
-#     root.geometry("+300+300")
-#     # Store the PhotoImage to prevent early garbage collection
-#     root.image = tk.PhotoImage(file="test.png")
-#     # Display the image on a label
-#     label = tk.Label(root, image=root.image)
-#     # Set the label background color to a transparent color
-#     label.config(bg='systemTransparent')
-#     label.pack()
-# elif platform == "win32":
-#     root.image = tk.PhotoImage(file='test.png')
-#     label = tk.Label(root, image=root.image, bg='white')
-#     root.overrideredirect(True)
-#     root.geometry("+250+250")
-#     root.lift()
-#     root.wm_attributes("-topmost", True)
-#     root.wm_attributes("-disabled", True)
-#     root.wm_attributes("-transparentcolor", "white")
-#     label.pack()
+if platform == "linux" or platform == "linux2":
+    root.overrideredirect(True)
+    root.wait_visibility(root)
+    root.wm_attributes("-alpha", 0.5)
+elif platform == "darwin":
+    root.overrideredirect(True)
+    # Make the root window always on top
+    root.wm_attributes("-topmost", True)
+    # Turn off the window shadow
+    root.wm_attributes("-transparent", True)
+    # Set the root window background color to a transparent color
+    root.config(bg='systemTransparent')
+    root.geometry("+300+300")
+    # Store the PhotoImage to prevent early garbage collection
+    root.image = tk.PhotoImage(file="test.png")
+    # Display the image on a label
+    label = tk.Label(root, image=root.image)
+    # Set the label background color to a transparent color
+    label.config(bg='systemTransparent')
+    label.pack()
+elif platform == "win32":
+    root.image = tk.PhotoImage(file='test.png')
+    label = tk.Label(root, image=root.image, bg='white')
+    root.overrideredirect(True)
+    root.geometry("+250+250")
+    root.lift()
+    root.wm_attributes("-topmost", True)
+    root.wm_attributes("-disabled", True)
+    root.wm_attributes("-transparentcolor", "white")
+    label.pack()
 
 
 
-# def ignore_event(event):
-#         return "break"
+def ignore_event(event):
+        return "break"
 
-# def mouse_event(event):
-#     print(1)
-#     root.withdraw()
-#     return "break"
+def mouse_event(event):
+    print(1)
+    root.withdraw()
+    return "break"
 
-# root.bind("<Button-1>", mouse_event)  # Ignore mouse clicks
-# root.bind("<Key>", ignore_event)  # Ignore key presses
-# root.bind("<Motion>", ignore_event)
+root.bind("<Button-1>", mouse_event)  # Ignore mouse clicks
+root.bind("<Key>", ignore_event)  # Ignore key presses
+root.bind("<Motion>", ignore_event)
 
-# l=tk.Label(root,text="HI this is an overlay",fg="white",font=(60),bg="red")     
-# l.pack() 
+l=tk.Label(root,text="HI this is an overlay",fg="white",font=(60),bg="red")     
+l.pack() 
 
 
 
@@ -145,20 +145,19 @@ print(5)
 time.sleep(0.5)
 
 # Keep the program running
-# root.mainloop()
 
 file.write("TESTING\n")
 
 
 
-try:
-    while 1:
-        if(started):
-            log("@>"+str(np.floor(time.time()*1000)) +">"+ str(grabpix(monitor["left"],monitor["top"])[2]==255))
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    file.close()
-    print("gracefully closed")
+# try:
+#     while 1:
+#         if(started):
+#             log("@>"+str(np.floor(time.time()*1000)) +">"+ str(grabpix(monitor["left"],monitor["top"])[2]==255))
+#         time.sleep(0.1)
+# except KeyboardInterrupt:
+#     file.close()
+#     print("gracefully closed")
 
 time.sleep(0.5)
 print(1)
@@ -174,5 +173,6 @@ print(3)
 
 
 
-
+root.mainloop()
+print("a")
 
