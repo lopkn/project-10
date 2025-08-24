02e80072ee597c177a70d37e60e480351270fa05
Lopkn <leoyuan06@gmail.com>
2025-08-23 21:35:09 -0700

    feat: repeater

--- repeater.sh
+++ repeater.sh
@@ -2,5 +2,9 @@ sleep 4;
 AWID=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
 while true;
 do 
-	xdotool mousedown 3; sleep 1.1; xdotool mouseup 3; sleep 0.2;
+        AWID2=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
+        if [[ "$AWID2" != "$AWID" ]]; then
+		exit 0;
+	fi
+	xdotool mousedown 3; sleep $1; xdotool mouseup 3; sleep 0.1;
 done
