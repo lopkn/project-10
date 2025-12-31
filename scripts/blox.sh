3f1a25e6fbad067acbf501c77925c2a07d51f3e6
Lopkn <leoyuan06@gmail.com>
2025-12-30 22:43:10 -0800

    chore: walk back

--- scripts/blox.sh
+++ scripts/blox.sh
@@ -1,2 +1,1 @@
-sleep 5 && xdotool mousedown 1 && sleep 1140 && xdotool keydown w && sleep 0.2 && xdotool keyup w
-
+sleep 5 && xdotool mousedown 1 && sleep 1140 && xdotool keydown w && sleep 0.2 && xdotool keyup w && xdotool keydown s && sleep 0.2 && xdotool keyup s
