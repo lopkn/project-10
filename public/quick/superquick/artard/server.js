72d682f36149842fcaff1468e269cdcd10eb25ec
Lopkn <leoyuan06@gmail.com>
2026-01-15 22:58:52 -0800

    chore: finally got p5js after years

---
+++ public/quick/superquick/artard/server.js
@@ -0,0 +1,25 @@
+﻿// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Server Error');
+            return;
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
