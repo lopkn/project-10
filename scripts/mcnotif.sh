462a5c94b576ed4e41496f749e69fb0d06b57f47
lopkn <leoyuan06@gmail.com>
2026-04-21 22:29:48 -0700

    fix: notifs

--- scripts/mcnotif.sh
+++ scripts/mcnotif.sh
@@ -5,6 +5,8 @@ CONTAINER="beautiful_turing"
 docker logs --tail 0 -f beautiful_turing | while read LINE; do
 
         # Send the push notification
-        curl -d "$LINE" "ntfy.sh/Lopkn_mc"
+        if [[ "$LINE" == *"joined"* ]] || [[ "$LINE" == *"left"* ]] || [[ "$LINE" == *"Secure"* ]]; then
+    		curl -d "$LINE" "ntfy.sh/Lopkn_mc"
+	fi
 
 done
